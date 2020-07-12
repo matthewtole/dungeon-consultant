9fe5d540407f70b2d0296abd58b92cab69d12364
Matthew Tole <matthewtole@gmail.com>
2020-07-12 13:16:46 -0700

    ✨ Add basic unarmed adventurer

---
+++ Assets/Code/Scripts/Characters/CharacterAnimation.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonManager {
+    public class CharacterAnimation : MonoBehaviour {
+        [SerializeField]
+        protected Animator animator;
+
+        [SerializeField]
+        protected string horizontalFloat = "Horizontal";
+        [SerializeField]
+        protected string verticalFloat = "Vertical";
+        [SerializeField]
+        protected string velocityFloat = "Velocity";
+        [SerializeField]
+        protected string idleBoolean = "Idle";
+        [SerializeField]
+        protected string attackTrigger = "Attack";
+        [SerializeField]
+        protected string hitTrigger = "Hit";
+        [SerializeField]
+        protected string actionTrigger = "Action";
+
+        protected float idleTimeout;
+        protected float idleTimer;
+
+        void Start() {
+            Stop();
+            IdleOff();
+            FaceDirection(Vector3.down);
+            EnableAutoIdle();
+        }
+        void Update() {
+            if (idleTimer > 0 && Time.time >= idleTimer) {
+                Idle();
+            }
+        }
+
+        public void FaceDirection(Vector3 position) {
+            animator.SetFloat(horizontalFloat, (position - transform.position).x);
+            animator.SetFloat(verticalFloat, (position - transform.position).y);
+        }
+
+        public void GetHit(Vector3 from) {
+            Stop();
+            IdleOff();
+            FaceDirection(from);
+            animator.SetTrigger(hitTrigger);
+            ResetIdleTimer();
+        }
+
+        public void Attack(Vector3 target) {
+            Stop();
+            IdleOff();
+            FaceDirection(target);
+            animator.SetTrigger(attackTrigger);
+            ResetIdleTimer();
+        }
+
+        public void Walk(Vector3 direction) {
+            IdleOff();
+            FaceDirection(direction);
+            animator.SetFloat(velocityFloat, 1f);
+            idleTimer = 0;
+        }
+
+        public void Stop() {
+            IdleOff();
+            animator.SetFloat(velocityFloat, 0f);
+            ResetIdleTimer();
+        }
+
+        public void Idle() {
+            Stop();
+            animator.SetBool(idleBoolean, true);
+            ResetIdleTimer();
+        }
+
+        public void IdleOff() {
+            animator.SetBool(idleBoolean, false);
+            ResetIdleTimer();
+        }
+
+        public void EnableAutoIdle(float timeout = 5f) {
+            idleTimeout = timeout;
+            ResetIdleTimer();
+        }
+
+        public void DisableAutoIdle() {
+            idleTimeout = 0;
+            ResetIdleTimer();
+        }
+
+        protected void ResetIdleTimer() {
+            if (idleTimeout > 0) {
+                idleTimer = Time.time + idleTimeout;
+            }
+            else {
+                idleTimeout = 0;
+            }
+        }
+    }
+
+}
---
+++ Assets/Documentation/animation_timings.md
@@ -0,0 +1,22 @@
+# Animation Timings
+
+## Character
+
+### Idle / Action
+
+Frame sequence: 1 2 3 2
+Timings: 0 0.40 0.45 1.25 1.30
+
+### Walk
+
+Frame sequence: 1 2 3 4
+Timings: 0 0.09 0.18 0.27 0.36
+
+### Attack
+
+
+
+### Hit
+
+Frame sequence: 1 2 3 2 3
+Timings: 0 0.07 0.12 0.17 0.22 0.27
