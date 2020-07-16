2927c2614d0f040d44c5cd9c634f32472bf2ee9a
Matthew Tole <matthewtole@gmail.com>
2020-07-16 16:54:28 -0700

    ✨ Add ShopEntries scriptable object

---
+++ Assets/Code/Scripts/Items/ShopEntries/ShopEntry.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.Items.ShopEntries
+{
+    [CreateAssetMenu]
+    public class ShopEntry : ScriptableObject
+    {
+        public GameObject prefab;
+        public int cost;
+        public Sprite shopImage;
+        public LayerMask buildLayers;
+        public string label;
+    }
+}
