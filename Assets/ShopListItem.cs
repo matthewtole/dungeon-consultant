5305ffc5db848e4330cea60a22ad85494586dc1b
Matthew Tole <matthewtole@gmail.com>
2020-07-16 17:11:44 -0700

    ✨ Build modal for the shop

---
+++ Assets/ShopListItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Code.Scripts.Items.ShopEntries;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ShopListItem : MonoBehaviour
+{
+    [SerializeField] protected ShopEntry entry;
+    [SerializeField] protected TMP_Text labelText;
+    [SerializeField] protected TMP_Text priceText;
+    [SerializeField] protected Image iconImage;
+    
+    void Start()
+    {
+    }
+    
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (entry && labelText && priceText && iconImage)
+        {
+            labelText.SetText(entry.label);
+            priceText.SetText(entry.cost.ToString());
+            iconImage.sprite = entry.shopImage;
+        }
+    }
+#endif
+}
