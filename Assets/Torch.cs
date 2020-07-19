0dd0d8aa8054183ca0389f7aaeae5eeda1dcd30f
Matthew Tole <matthewtole@gmail.com>
2020-07-18 21:02:04 -0700

    ✨ Add lighting and torches

---
+++ Assets/Torch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Torch : MonoBehaviour
+{
+    [SerializeField] protected Animator animator;
+    private static readonly int Lit = Animator.StringToHash("Lit");
+
+    void Start()
+    {
+        animator.SetBool(Lit, true);
+    }
+
+}
