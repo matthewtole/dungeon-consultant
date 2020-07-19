6d716da5bb5417c0eb690f8d939729de91273b92
Matthew Tole <matthewtole@gmail.com>
2020-07-19 11:18:33 -0700

    ♻️ Replace the build logic with the new move class

--- Assets/Code/Scripts/UI/ShopModal.cs
+++ Assets/Code/Scripts/UI/ShopModal.cs
@@ -7,18 +7,52 @@ namespace Code.Scripts.UI
     public class ShopModal : MonoBehaviour
     {
         [SerializeField] protected Modal modal;
-        [SerializeField] protected GameObject itemBuilderPrefab;
 
-        private GameObject _builder = null;
-
+        private ShopEntry _currentlyBuilding;
+        private GameObject _gameObject;
+        
         public void OnBuyItem(ShopEntry entry)
         {
-            if (_builder)
+            if (_currentlyBuilding != null)
             {
-                Destroy(_builder);
+                return;
             }
-            _builder = Instantiate(itemBuilderPrefab);
-            _builder.GetComponent<ItemBuilder>().Init(entry);
+            
+            _gameObject = Instantiate(entry.prefab);
+            MoveableItem moveable = _gameObject.GetComponent<MoveableItem>();
+            if (!moveable)
+            {
+                Debug.LogError("Tried to build an item that isn't moveable!");
+                Destroy(_gameObject);
+                return;
+            }
+
+            _currentlyBuilding = entry;
+            moveable.onMoveCompleted.AddListener(OnBuildComplete);
+            moveable.onMoveCancelled.AddListener(OnBuildCancel);
+            moveable.Move();
+            
+        }
+
+        private void OnBuildCancel()
+        {
+            _currentlyBuilding = null;
+            Destroy(_gameObject);
+        }
+
+        private void OnBuildComplete()
+        {
+            // TODO: Spend the money!
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                ShopEntry tmp = _currentlyBuilding;
+                _currentlyBuilding = null;
+                OnBuyItem(tmp);
+            }
+            else
+            {
+                _currentlyBuilding = null;
+            }
         }
     }
 }
